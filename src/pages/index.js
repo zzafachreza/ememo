dffe2876ff60ba8a2f74e007a5c8345a34988f7a
Fachreza Maulan <zzafachreza@gmail.com>
2021-09-25 00:42:44 +0700

    alhamdulillah

---
+++ src/pages/index.js
@@ -0,0 +1,57 @@
+import Splash from './Splash';
+import GetStarted from './GetStarted';
+import Register from './Register';
+import Login from './Login';
+import Home from './Home';
+import Account from './Account';
+import Success from './Success';
+import Success2 from './Success2';
+import Tambah from './Tambah';
+import ListDetail from './ListDetail';
+import Search from './Search';
+import Search2 from './Search2';
+import Kategori from './Kategori';
+import ListData from './ListData';
+import Notifikasi from './Notifikasi';
+import Laporan from './Laporan';
+import Schedule from './Schedule';
+import Results from './Results';
+import ScheduleDetail from './ScheduleDetail';
+import ResultsDetail from './ResultsDetail';
+import Artikel from './Artikel';
+import Player from './Player';
+import PlayerDetail from './PlayerDetail';
+import EditProfile from './EditProfile';
+
+import Wa from './Wa';
+
+import Infaq from './Infaq';
+
+export {
+  Splash,
+  Wa,
+  Infaq,
+  GetStarted,
+  Register,
+  Login,
+  Home,
+  Account,
+  Success,
+  Success2,
+  Tambah,
+  ListDetail,
+  Search,
+  Search2,
+  Kategori,
+  ListData,
+  Notifikasi,
+  Laporan,
+  Schedule,
+  Results,
+  Artikel,
+  ScheduleDetail,
+  ResultsDetail,
+  Player,
+  PlayerDetail,
+  EditProfile,
+};
---
+++ src/utils/colors/index.js
@@ -0,0 +1,12 @@
+export const colors = {
+  primary: '#0D965E',
+  secondary: '#1FA4B7',
+  tertiary: '#F59122',
+  background: '#6B63FF',
+  success: '#198754',
+  warning: '#F37120',
+  danger: '#dc3545',
+  black: 'black',
+  white: 'white',
+  border: '#7D8797',
+};
